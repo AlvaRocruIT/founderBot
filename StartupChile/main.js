9374b9c9f2dbce113b4dadd07b5d65fe75e9bc55
AlvaHRo <alvargash90@gmail.com>
2026-04-06 17:59:21 -0400

    Update main.js

--- StartupChile/main.js
+++ StartupChile/main.js
@@ -16,9 +16,9 @@ const loginEmail = document.getElementById("loginEmail");
 const startInput = document.getElementById("chat-input-start");
 const startSendBtn = document.getElementById("send-btn-start");
 const chatInput = document.getElementById("chat-input-chat");
-const chatSendBtn = document.getElementById("send-btn-chat");;
+const chatSendBtn = document.getElementById("send-btn-chat");
 
-const API_URL = https://founder-backend-yo6y.onrender.com/chat;
+const API_URL = "https://founder-backend-yo6y.onrender.com/chat";
 const nameRegex = /^[\p{L}]+(?:[\s'’\-][\p{L}]+)*$/u;
 const emailRegex = /^[A-Za-z0-9._%+-]+@([A-Za-z0-9-]{2,}\.)?[A-Za-z0-9-]{2,}\.[A-Za-z]{2,}$/;
 
